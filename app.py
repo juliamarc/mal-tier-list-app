d5aae20a61ca328e88bfa4b5ab5b569ce5e6d961
Julia Marcinkowska <jmarcinkowska@gmail.com>
2021-04-27 22:08:41 +0200

    add error handling and css

--- app.py
+++ app.py
@@ -1,40 +1,64 @@
 import os
 import uuid
 
-from flask import Flask, render_template, request
-import werkzeug
 
-from mal_tier_list_bbcode_gen.spreadsheetparser import SpreadsheetParser
+from flask import Flask, render_template, request
+
 from mal_tier_list_bbcode_gen.bbcodegenerator import BBCodeGenerator
+from mal_tier_list_bbcode_gen.image import GoogleDriveSourceError
+from mal_tier_list_bbcode_gen.spreadsheetparser import (
+    SpreadsheetParser, EntriesPerRowMissingError, EntriesPerRowNotANumberError,
+    HeaderIncompleteError)
+
 
 UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index.html', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         f = request.files.get('ods_file')
-        original_filename = werkzeug.utils.secure_filename(f.filename)
-        filename = '.'.join([str(uuid.uuid4()), 'ods'])
-        path = os.path.join('uploads', filename)
+        if not f:
+            return render_template('index.html', error_info='No file')
+        if f.mimetype != 'application/vnd.oasis.opendocument.spreadsheet':
+            return render_template('index.html', error_info='Wrong file type')
 
-        f.save(path)
+        stored_filename = os.path.join(app.config['UPLOAD_FOLDER'],
+                                       '.'.join([str(uuid.uuid4()), 'ods']))
 
-        parser = SpreadsheetParser(path)
-        parser.parse_tiers()
+        try:
+            f.save(stored_filename)
 
-        generator = BBCodeGenerator(parser.settings, parser.tiers)
-        generator.generate_bbcode()
+            parser = SpreadsheetParser(stored_filename)
+            parser.parse_tiers()
 
-        os.remove(path)
+            generator = BBCodeGenerator(parser.settings, parser.tiers)
+            generator.generate_bbcode()
 
-        html_preview = generator._generate_html_preview()
+            bbcode = generator.bbcode
+            html_preview = generator._generate_html_preview()
+        except (GoogleDriveSourceError,
+                EntriesPerRowMissingError,
+                EntriesPerRowNotANumberError,
+                HeaderIncompleteError,
+                KeyError) as e:
+            return render_template('index.html', error_info=str(e))
+        finally:
+            try:
+                os.remove(stored_filename)
+            except FileNotFoundError:
+                pass
+
         return render_template('result.html', html_preview=html_preview,
-                               bbcode=generator.bbcode)
+                               bbcode=bbcode)
 
-    return render_template('index.html', title='MALTierListApp')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
