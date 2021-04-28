ff821a65a3ee9195dac2548a5afd33e0564e1a11
Julia Marcinkowska <jmarcinkowska@gmail.com>
2021-04-28 10:55:37 +0200

    add css to error box, handle 413

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import uuid
 
+import click
+
 from werkzeug.exceptions import RequestEntityTooLarge
 from waitress import serve
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 from mal_tier_list_bbcode_gen.bbcodegenerator import BBCodeGenerator
 from mal_tier_list_bbcode_gen.image import GoogleDriveSourceError
@@ -22,7 +24,14 @@ app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 
 @app.errorhandler(RequestEntityTooLarge)
 def handle_bad_request(e):
-    return 'bad request!', 413
+    return render_template('index.html',
+                           error_info='File too large, max. 4MB'), 413
+
+
+@app.route('/favicon.ico')
+def fav():
+    return send_from_directory(app.root_path, 'favicon.ico')
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index.html', methods=['GET', 'POST'])
@@ -66,9 +75,17 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
+@click.command()
+@click.option('--dev', is_flag=True)
+def main(dev):
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
 
-    app.run(debug=True)
-    # serve(app, host="127.0.0.1", port=5000)
+    if dev:
+        app.run(debug=True)
+    else:
+        serve(app, host="0.0.0.0", port=5000)
+
+
+if __name__ == '__main__':
+    main()
